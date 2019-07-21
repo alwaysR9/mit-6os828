d947abf9cecfa2eb8c0b8415eb86fb73c9dd7b72
alwaysR9 <zhufangze123@gmail.com>
2019-07-21 18:14:10 +0800

    Lab1: finish Formatted Printing to the Console

--- kern/monitor.c
+++ kern/monitor.c
@@ -115,6 +115,18 @@ monitor(struct Trapframe *tf)
 	cprintf("Welcome to the JOS kernel monitor!\n");
 	cprintf("Type 'help' for a list of commands.\n");
 
+	int x = 1, y = 3, z = 4;
+	cprintf("x %d, y %x, z %d\n", x, y, z);
+
+	unsigned int i = 0x00646c72;
+	char* t = (char*) &i;
+	for (int k = 0; k < 4; ++ k) {
+		cprintf("%x:%c\n", (unsigned long)t, *t);
+		++ t;
+	}
+	cprintf("H%x Wo%s\n", 57616, &i);
+
+	cprintf("x=%d y=%d\n", 3);
 
 	while (1) {
 		buf = readline("K> ");
